54ad9971de4e5574c95fce10075e1b827533a458
JavierHdez <jhregalado@googlemail.com>
2017-11-08 20:02:44 +0100

    primer commit

---
+++ WebApplication1_clase7_peliculas_mvc/Controllers/inicialController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1_clase7_peliculas_mvc.Controllers
+{
+    //using System.Web;
+    //using System.Web.Mvc;
+
+    namespace MvcMovie.Controllers
+    {
+        public class HelloWorldController : Controller
+        {
+            // 
+            // GET: /HelloWorld/Welcome
+
+            public string Index()
+            {
+                return "This is my <b>default</b> action...";
+            }
+
+            // 
+            // GET: /HelloWorld/Welcome/ 
+
+            public string Welcome(string nombre, int veces=1)
+            {
+                return HttpUtility.HtmlEncode("Hello " + nombre + ", NumTimes is: " + veces);
+            }
+        }
+    }
+}
---
+++ WebApplication1_clase7_peliculas_mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication1_clase7_peliculas_mvc.MvcApplication" Language="C#" %>
